abee6af6304856b18ccd496293a48d2c6bd9d7c0
ricedev10 <ricedev10@gmail.com>
2025-01-23 22:19:05 -0500

    character development

--- code/player.gd
+++ code/player.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 1000.0
+const JUMP_VELOCITY = -1500.0
+const JUMP_GRAVITY = Vector2(0.0, 2100.0)
+const FALL_ACCELERATION = Vector2(0.0, 3280.0)
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y >= 0.0:
+			print("falling")
+			velocity += FALL_ACCELERATION * delta
+		else:
+			velocity += JUMP_GRAVITY * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
